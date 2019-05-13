24bced2e9eb9d9bbce29ac8f852295064dc9c059
kewenle <chunzhi.hu@allride.ai>
2019-05-13 18:42:37 +0800

    what

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
     print('hello world')
 
+def print_hello_new(nam):
+    print('hello ',nam)
+
 if __name__ == '__main__':
+    name = 'nanjing'
     print_hello()
+    print_hello_new(name)
